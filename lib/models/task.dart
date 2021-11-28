bab48bf85c98b957ffd7ad721ea067052136ecf8
mvmonica <monicavillanueva.shs@gmail.com>
2021-11-28 01:12:26 -0800

    updates and added task

---
+++ lib/models/task.dart
@@ -0,0 +1,10 @@
+
+class Task {
+  bool completed=false;
+  var title='';
+  var description='';
+  DateTime date;
+  var id=0;
+
+  Task({this.completed,this.title,this.description,this.date,this.id});
+
