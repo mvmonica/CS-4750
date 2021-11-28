83462e83b9b575694123deea9f720758015fa569
mvmonica <monicavillanueva.shs@gmail.com>
2021-11-28 01:05:10 -0800

    updates to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_calendar/calendar.dart';
-import 'package:flutter_calendar/todo.dart';
-import 'package:flutter_calendar/reminders.dart';
+import 'package:flutter_calendar/screens/home_page.dart';
+import 'package:flutter_calendar/screens/splash_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Calendar',
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _index = 0;
-
-  final List<Widget> _tabs = [Calendar(), Todo(), Reminders()];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _tabs[_index],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _index,
-        onTap: (index) => setState((){
-          _index = index;
-        }),
-        items: [
-        new BottomNavigationBarItem(
-          icon: Icon(Icons.calendar_today),
-          title: Text('Calendar'),
-        ), new BottomNavigationBarItem(
-          icon: Icon(Icons.checklist),
-          title: Text('To Do'),
-        ),
-        new BottomNavigationBarItem(
-          icon: Icon(Icons.alarm),
-          title: Text('Reminders'),
-        ),
-      ],
-      ),
-    );
-  }
-}
